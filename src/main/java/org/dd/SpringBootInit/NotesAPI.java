500b2c95c241808a586b9ff203189d5b690a71d7
SAMPADA <SAMPADA>
2018-10-13 15:57:51 +0530

    changed from standalone springboot app to web app

--- src/main/java/org/dd/SpringBootInit/NotesAPI.java
+++ src/main/java/org/dd/SpringBootInit/NotesAPI.java
@@ -2,9 +2,10 @@ package org.dd.SpringBootInit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NotesAPI {
+public class NotesAPI extends SpringBootServletInitializer{
 	
 	public static void main(String[] args){
 		
